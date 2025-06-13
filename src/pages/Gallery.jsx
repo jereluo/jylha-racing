be8f0ab81e89f1270dc940dbcacbd46a2de02372
jereluo <jere.luomanmaki@outlook.com>
2025-06-13 14:21:37 +0300

    lazy and pre load

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -6,17 +6,17 @@ const events = [
     title: 'XXX AKU Kevät23',
     thumbnail: '/jylha-racing/assets/thumbnails/ks23.png',
     images: [
-       '/jylha-racing/assets/images/KevätSlalom2023/1.jpg',
-       '/jylha-racing/assets/images/KevätSlalom2023/2.jpg',
-       '/jylha-racing/assets/images/KevätSlalom2023/3.jpg',
-       '/jylha-racing/assets/images/KevätSlalom2023/4.jpg',
-       '/jylha-racing/assets/images/KevätSlalom2023/5.jpg',
-       '/jylha-racing/assets/images/KevätSlalom2023/6.jpg',
-       '/jylha-racing/assets/images/KevätSlalom2023/7.jpg',
-       '/jylha-racing/assets/images/KevätSlalom2023/8.jpg',
-       '/jylha-racing/assets/images/KevätSlalom2023/9.jpg',
-       '/jylha-racing/assets/images/KevätSlalom2023/10.jpg',
-       '/jylha-racing/assets/images/KevätSlalom2023/11.jpg',
+      '/jylha-racing/assets/images/KevätSlalom2023/1.jpg',
+      '/jylha-racing/assets/images/KevätSlalom2023/2.jpg',
+      '/jylha-racing/assets/images/KevätSlalom2023/3.jpg',
+      '/jylha-racing/assets/images/KevätSlalom2023/4.jpg',
+      '/jylha-racing/assets/images/KevätSlalom2023/5.jpg',
+      '/jylha-racing/assets/images/KevätSlalom2023/6.jpg',
+      '/jylha-racing/assets/images/KevätSlalom2023/7.jpg',
+      '/jylha-racing/assets/images/KevätSlalom2023/8.jpg',
+      '/jylha-racing/assets/images/KevätSlalom2023/9.jpg',
+      '/jylha-racing/assets/images/KevätSlalom2023/10.jpg',
+      '/jylha-racing/assets/images/KevätSlalom2023/11.jpg',
     ],
   },
   {
@@ -24,31 +24,30 @@ const events = [
     title: 'XXVI AKU Syys22',
     thumbnail: '/jylha-racing/assets/thumbnails/ss22.png',
     images: [
-        '/jylha-racing/assets/images/SyysSlalom2022/1.JPG',
-        '/jylha-racing/assets/images/SyysSlalom2022/2.JPG',
-        '/jylha-racing/assets/images/SyysSlalom2022/3.JPG',
-        '/jylha-racing/assets/images/SyysSlalom2022/4.JPG',
-        '/jylha-racing/assets/images/SyysSlalom2022/5.JPG',
-        '/jylha-racing/assets/images/SyysSlalom2022/6.JPG',
-        '/jylha-racing/assets/images/SyysSlalom2022/7.JPG',
-        '/jylha-racing/assets/images/SyysSlalom2022/8.JPG',
-        '/jylha-racing/assets/images/SyysSlalom2022/9.JPG',
-        '/jylha-racing/assets/images/SyysSlalom2022/10.JPG',
-        '/jylha-racing/assets/images/SyysSlalom2022/11.JPG',
-        '/jylha-racing/assets/images/SyysSlalom2022/12.JPG',
-        '/jylha-racing/assets/images/SyysSlalom2022/13.JPG',
-        '/jylha-racing/assets/images/SyysSlalom2022/14.JPG',
+      '/jylha-racing/assets/images/SyysSlalom2022/1.JPG',
+      '/jylha-racing/assets/images/SyysSlalom2022/2.JPG',
+      '/jylha-racing/assets/images/SyysSlalom2022/3.JPG',
+      '/jylha-racing/assets/images/SyysSlalom2022/4.JPG',
+      '/jylha-racing/assets/images/SyysSlalom2022/5.JPG',
+      '/jylha-racing/assets/images/SyysSlalom2022/6.JPG',
+      '/jylha-racing/assets/images/SyysSlalom2022/7.JPG',
+      '/jylha-racing/assets/images/SyysSlalom2022/8.JPG',
+      '/jylha-racing/assets/images/SyysSlalom2022/9.JPG',
+      '/jylha-racing/assets/images/SyysSlalom2022/10.JPG',
+      '/jylha-racing/assets/images/SyysSlalom2022/11.JPG',
+      '/jylha-racing/assets/images/SyysSlalom2022/12.JPG',
+      '/jylha-racing/assets/images/SyysSlalom2022/13.JPG',
+      '/jylha-racing/assets/images/SyysSlalom2022/14.JPG',
     ],
   },
   {
     id: 3,
     title: 'Sekalaisia kuvia',
-    thumbnail: '/jylha-racing/assets/thumbnails/seka.png',
+    thumbnail: '/jylha-racing/assets/thumbnails/seka.jpg',
     images: [
-        '/jylha-racing/assets/images/Sekalaiset/1.jpeg',
-        '/jylha-racing/assets/images/Sekalaiset/2.jpg',
-        '/jylha-racing/assets/images/Sekalaiset/3.png',
-
+      '/jylha-racing/assets/images/Sekalaiset/1.jpeg',
+      '/jylha-racing/assets/images/Sekalaiset/2.jpg',
+      '/jylha-racing/assets/images/Sekalaiset/3.png',
     ],
   },
 ]
@@ -56,14 +55,17 @@ const events = [
 function Gallery() {
   const [selectedEvent, setSelectedEvent] = useState(null)
   const [selectedImageIndex, setSelectedImageIndex] = useState(0)
+  const [isFullscreen, setIsFullscreen] = useState(false)
 
   const openPopup = (event) => {
     setSelectedEvent(event)
     setSelectedImageIndex(0)
+    setIsFullscreen(false)
   }
 
   const closePopup = () => {
     setSelectedEvent(null)
+    setIsFullscreen(false)
   }
 
   const nextImage = () => {
@@ -76,6 +78,10 @@ function Gallery() {
     setSelectedImageIndex((prev) =>
       prev - 1 >= 0 ? prev - 1 : selectedEvent.images.length - 1
     )
+  }
+
+  const toggleFullscreen = () => {
+    setIsFullscreen(prev => !prev)
   }
 
   return (
@@ -97,6 +103,7 @@ function Gallery() {
               src={event.thumbnail}
               alt={event.title}
               className="thumbnail"
+              loading="lazy"
             />
           </div>
         ))}
@@ -112,12 +119,25 @@ function Gallery() {
             <img
               src={selectedEvent.images[selectedImageIndex]}
               alt={`${selectedEvent.title} kuva ${selectedImageIndex + 1}`}
-              className="popup-image"
+              className={`popup-image ${isFullscreen ? 'fullscreen-image' : ''}`}
+              onClick={toggleFullscreen}
+              loading="eager"
             />
-            <div className="popup-buttons">
-              <button onClick={prevImage} className="popup-button">Edellinen</button>
-              <button onClick={nextImage} className="popup-button">Seuraava</button>
-            </div>
+            
+            {selectedImageIndex + 1 < selectedEvent.images.length && (
+              <link
+                rel="preload"
+                as="image"
+                href={selectedEvent.images[selectedImageIndex + 1]}
+              />
+            )}
+
+            {!isFullscreen && (
+              <div className="popup-buttons">
+                <button onClick={prevImage} className="popup-button">Edellinen</button>
+                <button onClick={nextImage} className="popup-button">Seuraava</button>
+              </div>
+            )}
           </div>
         </div>
       )}
